2a3bb0a30f5f7436bbaf0f117ad4763288c76026
Jennifer <jt4nb@virginia.edu>
2021-11-25 17:00:32 -0500

    html for the homepage

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
     <head>
         <title> Testing </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
-    <body>
-        Hello World!
+    <body style="background-color:#434343">
+        <header>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php" style="color:red"><strong>Netflix Nowledge</strong></a>
+                    <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+            <div class="container">
+                <div class="row justify-content-center" style="margin-top:18%;">
+                    <a href="add.php" class="btn btn-danger" style="width:20%; padding:5% 0% 5% 0%; margin: 0% 3% 0% 3%"><strong>Add</strong></a>
+                    <a href="databaseView.php" class="btn btn-danger" style="width:20%; padding:5% 0% 5% 0%; margin: 0% 3% 0% 3%"><strong>Search, Delete, & Update</strong></a>
+                    <a href="sortAndFilter.php" class="btn btn-danger" style="width:20%; padding:5% 0% 5% 0%; margin: 0% 3% 0% 3%"><strong>Sort & Filter</strong></a>
+                  </div>
+                </div>
+            </div>
+        </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
