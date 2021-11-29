f5a2e957b04bc860a87846fffb2e77d02db0393f
RYCoop <77549175+RYCoop@users.noreply.github.com>
2021-11-29 15:07:37 -0500

    Update index.php
    Added check to see if logged in or not

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!DOCTYPE HTML>
-<html lang="en">
+<!DOCTYPE html>
+<html>
     <head>
         <title>Netflix Nowledge</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -9,12 +9,7 @@
             <nav class="navbar navbar-dark bg-dark">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.php" style="color:red"><strong>Netflix Nowledge</strong></a>
-                    <!--
-                    <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-danger" type="sort.filter">Search</button>
-                    </form>
-                    -->
+                    <a href="logout.php" class="btn btn-danger" style="color:white">Logout</a>
                 </div>
             </nav>
         </header>
@@ -31,11 +26,11 @@
 </html>
 
 <?php
-    require_once('./library.php');
-    $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo("Can't connect to MySQL Server. Error code: " . mysqli_connect_error());
-        return null;
-        }
+session_start();
+
+require_once "authCookieSessionValidate.php";
+
+if(!$isLoggedIn) {
+    header("Location: ./");
+}
 ?>
